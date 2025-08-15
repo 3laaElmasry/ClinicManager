b8dc91f0386b0ff1ce08f3491bbb4189e0c4f2ec
Elmasry <3laaelmasry2005a@gmail.com>
2025-08-15 15:42:23 +0300

    setup : Init Migration

--- ClinicManager.Infrastructure/DbContext/ApplicationDbContext.cs
+++ ClinicManager.Infrastructure/DbContext/ApplicationDbContext.cs
@@ -8,11 +8,20 @@ namespace ClinicManager.Infrastructure.DbContext
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser,ApplicationRole,Guid>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
-            : base (options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
-            
+
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+           
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
 
     }
 }
