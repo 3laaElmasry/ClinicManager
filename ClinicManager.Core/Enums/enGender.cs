18bcc4eba2e66d9506e042e8e01a58e9b241776a
Elmasry <3laaelmasry2005a@gmail.com>
2025-08-15 15:14:58 +0300

    init : Add Gender Enum

---
+++ ClinicManager.Core/Enums/enGender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManager.Core.Enums
+{
+    public enum enGender
+    {
+        Male = 1,
+        Female = 2,
+        Other = 3
+    }
+
+}
