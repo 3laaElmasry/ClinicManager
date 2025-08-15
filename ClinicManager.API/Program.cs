5d11d342ffe8453f1743dc5ead863579b9874823
Elmasry <3laaelmasry2005a@gmail.com>
2025-08-15 15:31:25 +0300

    setup: integrate Identity with ApplicationDbContext using EF Core stores

--- ClinicManager.API/Program.cs
+++ ClinicManager.API/Program.cs
@@ -1,5 +1,19 @@
 
+using ClinicManager.Infrastructure.DbContext;
+using ClinicManager.Infrastructure.DbContext.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(otpions =>
+{
+    otpions.UseSqlServer(builder.Configuration.GetConnectionString("cs"));
+});
+
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 
 builder.Services.AddControllers();
