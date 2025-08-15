02b4e16b5d2d4d40614134120ce3bab2e485fd5e
Elmasry <3laaelmasry2005a@gmail.com>
2025-08-15 15:35:45 +0300

    setup : Get  ApplicationDbContextOptions from injected options

--- ClinicManager.Infrastructure/DbContext/ApplicationDbContext.cs
+++ ClinicManager.Infrastructure/DbContext/ApplicationDbContext.cs
@@ -2,10 +2,17 @@
 
 using ClinicManager.Infrastructure.DbContext.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClinicManager.Infrastructure.DbContext
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser,ApplicationRole,Guid>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base (options)
+        {
+            
+        }
+
     }
 }
