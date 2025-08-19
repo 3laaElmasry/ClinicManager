5bfb8e8f1eec8ac517bdf20bb0d338c287f36caf
Elmasry <3laaelmasry2005a@gmail.com>
2025-08-19 22:39:05 +0300

    setup : IRepository Contract

---
+++ ClinicManager.Core/RepositoryContracts/IRepository.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.Linq.Expressions;
+
+namespace ClinicManager.Core.RepositoryContracts
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T,bool>>? filter = null, string? includeProprties = null);
+
+        Task<T?> GetAsync(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
+
+        Task AddAsync(T entity);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entities);
+
+        void Update(T entity);
+
+        Task Save();
+    }
+}
