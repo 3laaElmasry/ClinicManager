0ce2900bab61e0637c8129870f1d31a8a7d11f72
Elmasry <3laaelmasry2005a@gmail.com>
2025-08-20 00:03:31 +0300

    setup : Implment Repository Class

---
+++ ClinicManager.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,77 @@
+﻿
+using ClinicManager.Core.RepositoryContracts;
+using ClinicManager.Infrastructure.DbContext;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ClinicManager.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IQueryable<T>>? include = null,
+            bool tracked = false)
+        {
+            IQueryable<T> query = tracked ? _dbSet : _dbSet.AsNoTracking();
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            if (include != null)
+                query = include(query);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T?> GetAsync(
+            Expression<Func<T, bool>> filter,
+            Func<IQueryable<T>, IQueryable<T>>? include = null,
+            bool tracked = false)
+        {
+            IQueryable<T> query = tracked ? _dbSet : _dbSet.AsNoTracking();
+
+            query = query.Where(filter);
+
+            if (include != null)
+                query = include(query);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+
+}
