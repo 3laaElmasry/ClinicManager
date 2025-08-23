a8943b4d960e7eb82e3dd618d32c73812278f554
Elmasry <3laaelmasry2005a@gmail.com>
2025-08-23 19:09:01 +0300

    setup : Make one structure for all resonses by ResultClass

---
+++ ClinicManager.Core/Common/Result.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace ClinicManager.Core.Common
+{
+    public class Result<T>
+    {
+        public bool Success { get; private set; }
+        public string Message { get; private set; }
+        public List<T> Data { get; private set; }
+
+        private Result(bool success, string message, List<T> data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public static Result<T> SuccessResult(List<T> data, string message = "")
+            => new Result<T>(true, message, data);
+
+        // Overload for single item
+        public static Result<T> SuccessResult(T item, string message = "")
+            => new Result<T>(true, message, new List<T> { item });
+
+        public static Result<T> Failure(string message)
+            => new Result<T>(false, message, new List<T>());
+    }
+}
