780433f25d46e23de7ec5e18980098b0f6e36aee
Elmasry <3laaelmasry2005a@gmail.com>
2025-08-23 21:22:30 +0300

    setup : Patient Register Dto and customize Validations

---
+++ ClinicManager.Core/Common/Attriputes/MInAgeAttribute.cs
@@ -0,0 +1,29 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinicManager.Core.Common.Attriputes
+{
+    public class MinAgeAttribute : ValidationAttribute
+    {
+        private readonly int _minAge;
+
+        public MinAgeAttribute(int minAge,string userName = "Patient")
+        {
+            _minAge = minAge;
+
+            ErrorMessage = $"{userName} must be at least {_minAge} years old";
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if(value is not DateTime date)
+            {
+                return new ValidationResult("Ivalid date format.");
+            }
+
+            var age = DateTime.Now.Year - date.Year;
+
+            return age >= _minAge ? ValidationResult.Success : new ValidationResult(ErrorMessage);
+        }
+    }
+}
---
+++ ClinicManager.Core/DTO/User/RegisterPatientDto.cs
@@ -0,0 +1,49 @@
+﻿
+
+using ClinicManager.Core.Common.Attriputes;
+using ClinicManager.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinicManager.Core.DTO.User
+{
+    public class RegisterPatientDto
+    {
+
+
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50,MinimumLength = 3, ErrorMessage = "Full Name must be between {1} and {0} characters")]
+        public string FullName { get; set; } = string.Empty;
+
+        [MinAge(6)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        [EnumDataType(typeof(enGender), ErrorMessage = "Gender must be either Male or Female.")]
+        public enGender Gender { get; set; }
+
+
+        [StringLength(50)]
+        public string City { get; set; } = string.Empty;
+
+        [StringLength(50)]
+        public string Country { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d).{6,}$",
+    ErrorMessage = "Password must contain at least one letter and one number.")]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; } = string.Empty;
+
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        [StringLength(50)]
+        public string? InsuranceName { get; set; }
+
+    }
+
+}
