179d61c33e86db3ee7c29e06f6ca3f2a46c4b7ab
Elmasry <3laaelmasry2005a@gmail.com>
2025-08-23 18:19:42 +0300

    setup : Make A Genaric Logic Class In Services

---
+++ ClinicManager.Core/ServiceContracts/IGenericService.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.Linq.Expressions;
+
+namespace ClinicManager.Core.ServiceContracts
+{
+    public interface IGenericService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IQueryable<T>>? included = null,
+            bool tracked = false);
+
+        Task<T?> GetByIdAsync(Guid id,Func<IQueryable<T>,IQueryable<T>>? included = null, bool tracked = false);
+
+        Task<T> AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+    }
+
+}
---
+++ ClinicManager.Core/Services/GenericService.cs
@@ -0,0 +1,52 @@
+﻿
+
+using ClinicManager.Core.RepositoryContracts;
+using ClinicManager.Core.ServiceContracts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ClinicManager.Core.Services
+{
+    public class GenericService<T> : IGenericService<T> where T : class
+    {
+        private readonly IRepository<T> _repository;
+
+        public GenericService(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>,IQueryable<T>>? included = null,
+            bool tracked = false)
+        {
+            return await _repository.GetAllAsync(filter, included, tracked);
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id, Func<IQueryable<T>, IQueryable<T>>? included = null, bool tracked = false)
+        {
+            return await _repository.GetAsync(e => EF.Property<Guid>(e, "Id") == id, included, tracked);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _repository.AddAsync(entity);
+            await _repository.Save();
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _repository.Update(entity);
+            await _repository.Save();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _repository.Remove(entity);
+            await _repository.Save();
+        }
+    }
+
+}
