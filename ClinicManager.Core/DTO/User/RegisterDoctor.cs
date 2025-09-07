e2401974d5ca3a2eb6fd49906239efbe75d25923
Elmasry <3laaelmasry2005a@gmail.com>
2025-09-07 15:04:32 +0300

    setup : Implment Register Doctor

---
+++ ClinicManager.Core/DTO/User/RegisterDoctor.cs
@@ -0,0 +1,46 @@
+﻿
+
+using ClinicManager.Core.Common.Attriputes;
+using ClinicManager.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinicManager.Core.DTO.User
+{
+    public class RegisterDoctor
+    {
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between {1} and {0} characters")]
+        public string FullName { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        [MinAge(20)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        public enGender Gender { get; set; }
+
+
+        [StringLength(50)]
+        public string City { get; set; } = string.Empty;
+
+        [StringLength(50)]
+        public string Country { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(
+        pattern: @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$",
+    ErrorMessage = "Password must be at least 6 characters and include at least one uppercase, one lowercase.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        [StringLength(50)]
+        public string LicenseNumber { get; set; } = string.Empty;
+    }
+}
